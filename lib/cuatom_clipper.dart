da9e45e5b8114b6e40ab63d409c6eff9196850da
sazzad765 <shfahad.920@gmail.com>
2024-02-29 01:22:43 +0600

    first

---
+++ lib/cuatom_clipper.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(
+        size.width / 6, size.height - 80, size.width / 3, size.height - 60);
+
+    path.quadraticBezierTo(size.width - (size.width / 4), size.height - 20,
+        size.width, size.height - 100);
+
+    // path.quadraticBezierTo(
+    //     size.width-(size.width / 6), size.height , size.width, size.height - 80);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
+
+// class MyClipper extends CustomClipper<Path> {
+//   @override
+//   Path getClip(Size size) {
+//     var path = Path();
+//     path.lineTo(0, size.height - 80);
+//     path.quadraticBezierTo(
+//         size.width / 2, size.height, size.width, size.height - 80);
+//     path.lineTo(size.width, 0);
+//     path.close();
+//     return path;
+//   }
+//
+//   @override
+//   bool shouldReclip(CustomClipper<Path> oldClipper) {
+//     return false;
+//   }
+// }
